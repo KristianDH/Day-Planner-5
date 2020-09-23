496ec9ff8005bcd9674f0ce0d4bcd753b7e7b53b
kristiandh <kdwaynehill@gmail.com>
2020-09-23 08:13:16 -0400

    pushing completed files

---
+++ scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function (){
+
+var timeLabel = $('<span>')
+var textArea = $('<textarea>')
+var saveBtn = $('<button>')
+
+// Currrent date and time
+
+//Append 
+$('time-container').append(timeLabel, textArea, saveBtn)
+
+
+
+})
+
+
+
+
